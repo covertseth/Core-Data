3ebcf984b77570affe47ea7534c9fad95cdad9db
covertseth <40729060+covertseth@users.noreply.github.com>
2019-05-10 21:31:45 -0500

    Add files via upload

---
+++ CoreData/CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Seth Covert on 5/10/19.
+//  Copyright © 2019 Seth Covert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
